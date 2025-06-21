426a5140210dc468d023c20528115256a2cb3805
siddhiisbm <webdevelopment@isbm.org.in>
2025-06-21 15:04:57 +0530

    add env

--- resources/views/layouts/frame.blade.php
+++ resources/views/layouts/frame.blade.php
@@ -16,7 +16,26 @@
 <body>
     <!-- Preloader -->
     <div class="preloader">
-        <img src="assets/images/logos/favicon.png" alt="loader" class="lds-ripple img-fluid" />
+        <div class="scene">
+            <div class="shadow"></div>
+            <div class="jumper">
+                <div class="spinner">
+                    <div class="scaler">
+                        <div class="loader">
+                            <div class="cuboid">
+                                <div class="cuboid__side"></div>
+                                <div class="cuboid__side"></div>
+                                <div class="cuboid__side"></div>
+                                <div class="cuboid__side"></div>
+                                <div class="cuboid__side"></div>
+                                <div class="cuboid__side"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
     <div id="main-wrapper">
         <!-- Navbar Start -->
