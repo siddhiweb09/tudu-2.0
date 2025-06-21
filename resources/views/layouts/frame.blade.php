df2dcfc245627896f49fdb2bc9a4c4e78c831871
siddhiisbm <webdevelopment@isbm.org.in>
2025-06-21 15:41:17 +0530

    web file

--- resources/views/layouts/frame.blade.php
+++ resources/views/layouts/frame.blade.php
@@ -44,7 +44,7 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex flex-wrap">
             <div class="col-12">
                 <div class="container">
-                    <a class="navbar-brand" href="#"><img src=""/></a>
+                    <a class="navbar-brand" href="#"><img src="assets/images/logo.png" class="logo"/></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
