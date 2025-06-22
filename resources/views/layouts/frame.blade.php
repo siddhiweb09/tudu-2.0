393b0534575ea25bb6ad7fa8102afd10fee033d7
siddhiisbm <webdevelopment@isbm.org.in>
2025-06-22 12:38:39 +0530

    button css

--- resources/views/layouts/frame.blade.php
+++ resources/views/layouts/frame.blade.php
@@ -9,6 +9,8 @@
     <link rel="shortcut icon" type="image/png" href="assets/images/logos/favicon.png" />
     <!-- Core Css -->
     <link rel="stylesheet" href="assets/css/styles.css" />
+    <link rel="stylesheet" href="assets/css/buttons.css" />
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
